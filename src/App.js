477a1869afd72275595b7e642a9bb9d1767ad8cb
Sudhakar Kumar <sudhakar.kumar@shoesonloose.com>
2019-03-18 13:01:54 +0530

    comment console

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   constructor(){
 	this.state = {}
-        console.log("logging ",this.state)
+        //console.log("logging ",this.state)
   }
   render() {
     return (
